3cf3dc2f17dc8ee56aa9efba09732a665c298ced
celer-y <karleugene.eguia@ama.edu.ph>
2023-04-07 11:47:47 +0800

    Add register form

--- login.py
+++ login.py
@@ -11,7 +11,7 @@ class Login:
         self.root.geometry("1366x768")
 
         # Load background image
-        self.bg_image = ImageTk.PhotoImage(Image.open("src/login.png"))
+        self.bg_image = ImageTk.PhotoImage(Image.open("src/login/login.png"))
 
         canvas = tk.Canvas(self.root, width=1366, height=768)
         canvas.pack(fill="both", expand=True)
@@ -19,23 +19,23 @@ class Login:
 
         font_style = ("Rubik", 16)
 
-        self.username_entry = tk.Entry(self.root, width=30, bg="#D9D9D9", bd=0, highlightthickness=0, font=font_style)
+        self.username_entry = tk.Entry(self.root, width=30, bg="#D8DEE9", bd=0, highlightthickness=0, font=font_style)
         self.username_entry.place(x=571, y=288, width=316, height=60)
 
-        self.password_entry = tk.Entry(self.root, show="*", width=30, bg="#D9D9D9", bd=0, highlightthickness=0, font=font_style)
+        self.password_entry = tk.Entry(self.root, show="*", width=30, bg="#D8DEE9", bd=0, highlightthickness=0, font=font_style)
         self.password_entry.place(x=571, y=384, width=316, height=60)
 
-        login_button = tk.Button(self.root, text="Login", bg="#3E806E", fg="#D9D9D9", bd=0, highlightthickness=0, font=font_style, command=self.login)
+        login_button = tk.Button(self.root, text="Login", bg="#3E806E", fg="#ECEFF4", bd=0, highlightthickness=0, font=font_style, command=self.login)
         login_button.place(x=479, y=509, width=408, height=60)
 
-        canvas.create_rectangle(479, 288, 571, 348, fill="#B97A7A", outline="")
-        canvas.create_rectangle(479, 384, 571, 444, fill="#B97A7A", outline="")
+        canvas.create_rectangle(479, 288, 571, 348, fill="#BF616A", outline="")
+        canvas.create_rectangle(479, 384, 571, 444, fill="#BF616A", outline="")
 
-        username_icon = Image.open("src/icon_username.png")
+        username_icon = Image.open("src/login/icon_username.png")
         username_photo = ImageTk.PhotoImage(username_icon)
         canvas.create_image(511, 302, image=username_photo, anchor="nw")
 
-        password_icon = Image.open("src/icon_password.png")
+        password_icon = Image.open("src/login/icon_password.png")
         password_photo = ImageTk.PhotoImage(password_icon)
         canvas.create_image(511, 398, image=password_photo, anchor="nw")
 
